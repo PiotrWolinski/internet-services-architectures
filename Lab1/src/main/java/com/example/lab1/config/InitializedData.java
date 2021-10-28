694741874056b6a31c130174ed09c58d8f2505cb
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2021-10-28 14:55:34 +0200

    feat: refactored data initializer + added new requests

--- Lab1/src/main/java/com/example/lab1/config/InitializedData.java
+++ Lab1/src/main/java/com/example/lab1/config/InitializedData.java
@@ -28,9 +28,22 @@ public class InitializedData {
         this.engineService = engineService;
         this.userService = userService;
     }
-    // Podziele na osobne funkcje do inicjalizacji -> done
+
     @PostConstruct
-    private synchronized void init() {
+    private synchronized void initEngines() {
+        Engine weakPetrol = Engine.builder().name("35 TFSI").displacement(1.5).build();
+        Engine strongPetrol = Engine.builder().name("55 TFSI").displacement(3.0).build();
+        Engine strongestPetrol = Engine.builder().name("V8 Biturbo").displacement(4.0).build();
+        Engine weakDiesel = Engine.builder().name("2.0 TDI").displacement(2.0).build();
+
+        engineService.create(weakPetrol);
+        engineService.create(strongPetrol);
+        engineService.create(strongestPetrol);
+        engineService.create(weakDiesel);
+    }
+
+    @PostConstruct
+    private synchronized void initUsers() {
         User admin = User.builder()
                 .login("admin")
                 .name("John")
@@ -49,22 +62,25 @@ public class InitializedData {
                 .password(Sha256Utility.hash("sTR0nG-p4ssW0rD"))
                 .build();
 
+        User emptyUser = User.builder()
+                .login("looser")
+                .name("John")
+                .surname("Hill")
+                .birthDate(LocalDate.of(2002, 6, 12))
+                .email("john@hill.com")
+                .password(Sha256Utility.hash("sTR0nG-p4ssW0rD"))
+                .build();
+
         userService.create(admin);
         userService.create(mat);
+        userService.create(emptyUser);
+    }
 
-        Engine weakPetrol = Engine.builder().name("35 TFSI").displacement(1.5).build();
-        Engine strongPetrol = Engine.builder().name("55 TFSI").displacement(3.0).build();
-        Engine strongestPetrol = Engine.builder().name("V8 Biturbo").displacement(4.0).build();
-        Engine weakDiesel = Engine.builder().name("2.0 TDI").displacement(2.0).build();
-
-        engineService.create(weakPetrol);
-        engineService.create(strongPetrol);
-        engineService.create(strongestPetrol);
-        engineService.create(weakDiesel);
-
+    @PostConstruct
+    private synchronized void initCars() {
         Car audiA6 = Car.builder()
-                .engine(strongPetrol)
-                .user(mat)
+                .engine(engineService.find("55 TFSI").get())
+                .user(userService.find("CarWow").get())
                 .doors(5)
                 .name("Audi A6")
                 .wheels(4)
@@ -73,8 +89,8 @@ public class InitializedData {
                 .build();
 
         Car audiQ8 = Car.builder()
-                .engine(strongestPetrol)
-                .user(admin)
+                .engine(engineService.find("V8 Biturbo").get())
+                .user(userService.find("admin").get())
                 .doors(5)
                 .name("Audi RSQ8")
                 .wheels(4)
@@ -83,8 +99,8 @@ public class InitializedData {
                 .build();
 
         Car opel = Car.builder()
-                .engine(weakDiesel)
-                .user(admin)
+                .engine(engineService.find("2.0 TDI").get())
+                .user(userService.find("admin").get())
                 .doors(5)
                 .name("Opel Insignia")
                 .wheels(4)
@@ -92,8 +108,19 @@ public class InitializedData {
                 .maxSpeed(200)
                 .build();
 
+        Car fabia = Car.builder()
+                .engine(engineService.find("2.0 TDI").get())
+                .user(userService.find("admin").get())
+                .doors(3)
+                .name("Skoda Fabia")
+                .wheels(4)
+                .seats(4)
+                .maxSpeed(170)
+                .build();
+
         carService.create(audiA6);
         carService.create(audiQ8);
         carService.create(opel);
+        carService.create(fabia);
     }
 }
